c99b3049c01c7de85ec14083321ce69a3eb1e4e8
Ivo <github@thedeans.id.au>
2023-06-07 15:39:04 +0800

    This uses a simple multipart  config with a trivial template for cloud-config as well

--- cloud-init-yml-config.tf
+++ cloud-init-yml-config.tf
@@ -1,4 +1,4 @@
-data "cloudinit_config" "server_config" {
+/* data "cloudinit_config" "server_config" {
   gzip          = true
   base64_encode = true
   part {
@@ -8,4 +8,20 @@ data "cloudinit_config" "server_config" {
     content = file("cloud-init/cloud-init-non-docker.yml")
   }
 }
+*/
 
+data "cloudinit_config" "server_config" {
+  gzip          = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content = templatefile("cloud-init/cloud-config.tftpl", {
+      pkgs = ["nginx", "git"]
+    })
+  }
+
+  part {
+    content_type = "text/x-shellscript"
+    content = file("scripts/setup-script.sh")
+  }
+}
