f57e8cd428f615424fc18e14c5dba91cc8955df6
Ivo <github@thedeans.id.au>
2023-06-07 14:53:09 +0800

    A simple non Docker nginx install is even easier to configure

--- cloud-init-yml-config.tf
+++ cloud-init-yml-config.tf
@@ -3,6 +3,7 @@ data "cloudinit_config" "server_config" {
   base64_encode = true
   part {
     content_type = "text/cloud-config"
+    # The initial cloud-init.yml uses mostly runcmd to run a Docker nginx server
     #content = file("cloud-init/cloud-init.yml")
     content = file("cloud-init/cloud-init-non-docker.yml")
   }
